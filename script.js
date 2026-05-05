dbfd321ee0514e8f74551f6fcb87c2bd617e2f2f
Ben H <ben.helfstein@icloud.com>
2026-05-05 09:40:24 -0400

    magical wizard wacky website

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+//boolean for wacky mode
+let wackyMode = false;
+
+/*
+NEW JS FUNTION:
+document.querySelector();
+lets you select an html element and store the refrence in a variable for js
+*/
+
+const body = document.querySelector("body");
+const heading = document.querySelector("#main-heading");
+const description = document.querySelector("description");
+const toggleBtn = document.querySelector("#toggle");
+
+//JS can now change atributes and properties
+heading.textContent = "The Mostest Increable Website (Now with Javascript)"
+heading.style.color = "white";
