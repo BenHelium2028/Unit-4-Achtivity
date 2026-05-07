3f65e6ca1811848a79c5ed5e0c843162ffc4dd53
Ben H <ben.helfstein@icloud.com>
2026-05-07 12:10:11 -0400

    project

--- script.js
+++ script.js
@@ -9,9 +9,97 @@ lets you select an html element and store the refrence in a variable for js
 
 const body = document.querySelector("body");
 const heading = document.querySelector("#main-heading");
-const description = document.querySelector("description");
+const description = document.querySelector("#description");
 const toggleBtn = document.querySelector("#toggle");
 
+// rainbow colors
+const colors = [
+    "#ff0000",
+    "#ff7300",
+    "#fffb00",
+    "#48ff00",
+    "#00ffd5",
+    "#002bff",
+    "#7a00ff",
+    "#ff00c8"
+];
+
+let colorIndex = 0;
+let colorInterval;
+
 //JS can now change atributes and properties
-heading.textContent = "The Mostest Increable Website (Now with Javascript)"
+
+//initial:
+body.style.background = "#212529";
+
+heading.textContent = "The Mostest Increable Website (Now with Javascript)";
 heading.style.color = "white";
+
+description.style.color = "white";
+
+toggleBtn.style.color = "white";
+toggleBtn.style.backgroundColor = "#212529";
+toggleBtn.style.border = "1px solid white";
+toggleBtn.style.borderRadius = "12px";
+toggleBtn.style.padding = "8px 16px";
+toggleBtn.style.cursor = "pointer";
+
+function toggleMode() {
+    wackyMode = !wackyMode;
+
+    if (wackyMode == false) {
+
+        // stop rainbow effect
+        clearInterval(colorInterval);
+
+        // REMOVE smooth transition
+        body.style.transition = "none";
+
+        body.style.background = "#212529";
+
+        heading.textContent = "The Mostest Increable Website (Now with Javascript)";
+        heading.style.color = "white";
+
+        description.style.color = "white";
+
+        toggleBtn.style.color = "white";
+        toggleBtn.style.backgroundColor = "#212529";
+        toggleBtn.style.border = "1px solid white";
+        toggleBtn.style.borderRadius = "12px";
+        toggleBtn.style.padding = "8px 16px";
+        toggleBtn.style.cursor = "pointer";
+
+        //bug fixing:
+        console.log("Wacky Mode Set:");
+        console.log(wackyMode);
+
+    } 
+    else {
+
+        heading.style.color = "#212529";
+        description.style.color = "#212529";
+
+        toggleBtn.style.color = "white";
+        toggleBtn.style.backgroundColor = "#212529";
+        toggleBtn.style.border = "1px solid white";
+        toggleBtn.style.borderRadius = "12px";
+        toggleBtn.style.padding = "8px 16px";
+        toggleBtn.style.cursor = "pointer";
+
+        // REMOVE smooth transition
+        body.style.transition = "none";
+
+        // start rainbow effect
+        colorInterval = setInterval(() => {
+            body.style.backgroundColor = colors[colorIndex];
+            colorIndex = (colorIndex + 1) % colors.length;
+        }, 100);
+
+        //bug fixing:
+        console.log("Wacky Mode Set:");
+        console.log(wackyMode);
+    }
+}
+
+//atach funtion to button
+toggleBtn.addEventListener("click", toggleMode);
